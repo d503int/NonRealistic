6479b9e4199b3b713a3e9b656e6ba8731aebae42
Boris  Zhdanov <bzhdanov@ipolecat.com>
2016-05-23 11:51:19 +0300

    sepia filter added

--- CoreImageTesting.playground/Contents.swift
+++ CoreImageTesting.playground/Contents.swift
@@ -1,5 +1,15 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
+import CoreImage
 
-var str = "Hello, playground"
+let context = CIContext(options: nil)
+let url = NSURL(string: "https://upload.wikimedia.org/wikipedia/ru/2/24/Lenna.png")
+let image = CIImage(contentsOfURL: url!)
+
+
+// MARK: Sepia filter
+let sepiaFilter = CIFilter(name: "CISepiaTone")
+sepiaFilter?.setValue(image, forKey: kCIInputImageKey)
+sepiaFilter?.setValue(0.5, forKey: kCIInputIntensityKey)
+
+let result = sepiaFilter?.valueForKey(kCIOutputImageKey)
